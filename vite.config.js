2d745ef9df5a5f3567f09f5eded8ad7e31fde026
ASHISH KUMAR TIWARI <ashishkumartiwari641@gmail.com>
2026-01-04 20:09:52 +0530

    Clean up vite.config.js configuration
    Removed comment about the base path and ensured proper formatting.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Lagaan2.O/',
+  base: '/Lagaan2.O/', // <--- Ye alphabet 'O' hai, ise check kar lein
 })
